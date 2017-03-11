deab6c4e7e0fe0613d8e6a62e5ef2370516efb96
ThinkingInJava <yuyong@renaissance-era.com>
2017-03-11 19:28:19 +0800

    增加MaxTree算法

--- src/jobalgorithm.java
+++ src/jobalgorithm.java
@@ -1,7 +1,4 @@
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Stack;
+import java.util.*;
 
 /**
  * Created by ilove on 03/03/2017.
@@ -146,7 +143,92 @@ public class jobalgorithm {
         return res;
     }
 
+    /**
+     * note:每一个数的父节点是它左边第一个比它大的数和它右边第一个比它大的数中较小的那个
+     * title：
+     * 数组没有重复元素
+     * MaxTree是一颗二叉树，数组的每一个值对应的一个二叉树节点
+     * 包括MaxTree树在内且在其中的每一颗子树上，值最大的节点都是树的头
+     *    给定一个没有重复元素的数组arr，写出生成这个数组的MaxTree的函数，要求如果数组长度为N，
+     *    则时间复杂度为O(N)，额外空间复杂度为O(N).
+     * @param args
+     */
+    class Node{
+        public int value;
+        public Node left;
+        public Node right;
 
+        public Node(int data){
+            this.value=data;
+        }
+    }
+
+    public Node getMaxTree(int[] arr){
+        Node[] nArr = new Node[arr.length];
+        for(int i=0;i!= arr.length;i++){
+            nArr[i] = new Node(arr[i]);
+        }
+        Stack<Node> stack= new Stack<>();
+        HashMap<Node,Node> lBigMap = new HashMap<>();
+        HashMap<Node,Node> rBigMao = new HashMap<>();
+        for(int i=0;i!=nArr.length;i++){
+            Node curNode=nArr[i];
+            while(!stack.isEmpty() && stack.peek().value<curNode.value){
+                popStackSetMap(stack,lBigMap);
+            }
+            stack.push(curNode);
+        }
+        while(!stack.isEmpty()){
+            popStackSetMap(stack,lBigMap);
+        }
+        for (int i=nArr.length-1;i != -1;i--){
+            Node curNode = nArr[i];
+            while(!stack.isEmpty() && stack.peek().value< curNode.value ){
+                popStackSetMap(stack,rBigMao);
+            }
+            stack.push(curNode);
+        }
+        while(!stack.isEmpty()){
+            popStackSetMap(stack,rBigMao);
+        }
+        Node head=null;
+        for (int i=0;i != nArr.length;i++){
+            Node curNode = nArr[i];
+            Node left = lBigMap.get(curNode);
+            Node right = rBigMao.get(curNode);
+            if (,left == null && right == null){
+                head = curNode;
+            }else if(left==null){
+                if (right.left==null){
+                    right.left=curNode;
+                }else{
+                    right.right=curNode;
+                }
+            }else if(right==null){
+                if (left.left==null){
+                    left.left=curNode;
+                }else{
+                    left.right=curNode;
+                }
+            }else{
+                Node parent = left.value < right.value ? left:right;
+                if (parent.left == null){
+                    parent.left = curNode;
+                }else{
+                    parent.right = curNode;
+                }
+            }
+        }
+        return head;
+    }
+    public void popStackSetMap(Stack<Node> stack,HashMap<Node,Node> map){
+        Node popNode = stack.pop();
+        if (stack.isEmpty()){
+            map.put(popNode,null);
+        }else {
+            map.put(popNode,stack.peek());
+        }
+    }
     public static void main(String args[]){
         Stack<Integer> stack=new Stack<>();
         stack.push(1);
