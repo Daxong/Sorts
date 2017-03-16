b94fe9809e79393242bb97eca392e64ccd7434c6
ThinkingInJava <yuyong@renaissance-era.com>
2017-03-16 18:18:34 +0800

    反转单项链表～

--- src/StcakAndQueue.java
+++ src/StcakAndQueue.java
@@ -3,9 +3,9 @@ import java.util.*;
 /**
  * Created by ilove on 03/03/2017.
  *
- *
+ *Error:(8, 8) java: class StackAndQueue is public, should be declared in a file named StackAndQueue.java
  */
-public class jobalgorithm {
+public class StcakAndQueue {
     /**
      * 一个栈一次压入1，2，3，4，5，那么从栈顶到栈底分别为5，4，3，2，1。
      * 将这个栈转置后，从栈顶到栈底为1，2，3，4，5，也就是实现栈中元素的逆序，
@@ -265,7 +265,7 @@ public class jobalgorithm {
         }
     }
     public static void main(String args[]){
-        jobalgorithm job=new jobalgorithm();
+        StcakAndQueue job=new StcakAndQueue();
         job.getMaxTree(new int[]{1,3,2,4,9,6,5});
     }
 }
